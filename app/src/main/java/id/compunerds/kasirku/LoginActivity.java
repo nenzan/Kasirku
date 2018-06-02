28f417c7e3f96e9d00976511c910c71ec5a87d07
nenza <nenza.nurfirmansyah@student.unjani.ac.id>
2018-06-02 18:11:23 +0700

    Commit pertama, share project.

---
+++ app/src/main/java/id/compunerds/kasirku/LoginActivity.java
@@ -0,0 +1,32 @@
+package id.compunerds.kasirku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void init() {
+        login = (Button) findViewById(R.id.loginButton);
+    }
+}
